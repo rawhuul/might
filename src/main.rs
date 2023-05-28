07c4a9390171c44cd0cd22fc45c8fc5554456479
Rahul <basicfunc@gmail.com>
2023-05-28 20:12:25 +0530

    Added rustyline for better experience

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,82 @@
 use std::io::{self, Write};
 use reqwest::blocking::Client;
-use serde_json::Value;
+use serde_json::{json, Value};
+use rustyline::{DefaultEditor, Result};
+use rustyline::error::ReadlineError;
 
 fn main() {
+    let mut rl = DefaultEditor::new().expect("Error Occured");
     loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
+        let readline = rl.readline(">>> ");
+        match readline {
+            Ok(input) => {
+                let input = input.trim();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+                if input.is_empty() {
+                    continue;
+                }
 
-        let input = input.trim();
+                if input == "exit" {
+                    break;
+                }
 
-        if input.is_empty() {
-            continue;
-        }
+                let mut parts = input.splitn(2, ' ');
+                let method = parts.next().unwrap();
+                let url = parts.next().unwrap();
 
-        if input == "exit" {
-            break;
-        }
+                let response = match method {
+                    "GET" => send_get_request(url),
+                    "POST" => {
+                        print!("Body: ");
+                        io::stdout().flush().unwrap();
 
-        let mut parts = input.splitn(2, ' ');
-        let method = parts.next().unwrap();
-        let url = parts.next().unwrap();
+                        let mut body = String::new();
+                        io::stdin().read_line(&mut body).unwrap();
 
-        let response = match method {
-            "GET" => send_get_request(url),
-            "POST" => {
-                print!("Body: ");
-                io::stdout().flush().unwrap();
+                        send_post_request(url, &body)
+                    }
+                    _ => {
+                        println!("Invalid method: {}", method);
+                        continue;
+                    }
+                };
 
-                let mut body = String::new();
-                io::stdin().read_line(&mut body).unwrap();
+                match response {
+                    Ok(json) => {
+                        let pretty_json = serde_json::to_string_pretty(&json).unwrap();
+                        println!("{}", pretty_json);
+                    }
+                    Err(err) => {
+                        println!("Error: {}", err);
+                    }
+                }
+            },
 
-                send_post_request(url, &body)
-            }
-            _ => {
-                println!("Invalid method: {}", method);
-                continue;
-            }
-        };
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
 
-        match response {
-            Ok(json) => {
-                let pretty_json = serde_json::to_string_pretty(&json).unwrap();
-                println!("{}", pretty_json);
-            }
             Err(err) => {
-                println!("Error: {}", err);
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
 }
 
-fn send_get_request(url: &str) -> Result<Value, Box<dyn std::error::Error>> {
+fn send_get_request(url: &str) -> std::result::Result<Value, Box<dyn std::error::Error>> {
     let client = Client::new();
     let response = client.get(url).send()?;
     let json: Value = response.json()?;
     Ok(json)
 }
 
-fn send_post_request(url: &str, body: &str) -> Result<Value, Box<dyn std::error::Error>> {
+fn send_post_request(url: &str, body: &str) -> std::result::Result<Value, Box<dyn std::error::Error>> {
     let client = Client::new();
     let response = client.post(url).body(body.to_string()).send()?;
     let json: Value = response.json()?;
